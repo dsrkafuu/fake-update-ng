f04a4dd6b18197fcf79faeb63e3c3de2f071a4f3
amzrk2 <amzrk2@outlook.com>
2020-07-01 14:50:03 +0800

    Add Catalina updating page

--- README.md
+++ README.md
@@ -1,24 +1,14 @@
 # fake-update
 
-## Project setup
-```
-npm install
+## Development setup
+
+```bash
+npm install    # Install dependencies
+npm run serve  # Compiles and hot-reloads for development
+npm run build  # Compiles and minifies for production
+npm run lint   # Lints and fixes files
 ```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+## Data sources
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+- Apple logo: https://github.com/gilbarbara/logos/blob/master/logos/apple.svg
--- src/views/systems/macos-catalina.vue
+++ src/views/systems/macos-catalina.vue
@@ -1,12 +1,102 @@
 <template>
-  <div class="macos-catalina">macos-catalina</div>
+  <div class="macos-catalina">
+    <div class="apple-logo">
+      <apple-logo></apple-logo>
+    </div>
+    <div class="progress-bar" ref="progressBar">
+      <div class="progress-bar-placeholder"></div>
+      <div class="progress-bar-progress" :style="progress"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import AppleLogo from '@/assets/svgs/AppleLogo.vue';
+import _ from '@/default.js';
+
 export default {
   name: 'macos-catalina',
+  components: {
+    AppleLogo,
+  },
+  data() {
+    return {
+      timer: _.timer,
+      progress: {
+        width: '0',
+        transition: 'none',
+      },
+      progressInterval: undefined,
+    };
+  },
+  methods: {
+    updateProgress(animationTime) {
+      // Add transition CSS according to timer
+      this.progress.transition = `width ${animationTime}ms ease`;
+      // Delay the change of width after 4ms (DOM_MIN_TIMEOUT_VALUE)
+      // To prevent CSS transition not work properly
+      setTimeout(() => {
+        this.progress.width = '100%';
+      }, 4);
+    },
+  },
+  mounted() {
+    // Load timer from store
+    if (this.$store.state.timer !== this.timer) {
+      this.timer = this.$store.state.timer;
+    }
+    const animationTime = this.timer * 60 * 1000; // Animation time (ms)
+    this.updateProgress(animationTime);
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.macos-catalina {
+  background-color: #000000;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.apple-logo {
+  position: absolute;
+  width: 3.75rem;
+  color: #ffffff;
+
+  svg {
+    width: 100%;
+  }
+}
+
+.progress-bar {
+  position: relative;
+  top: 6.3rem;
+  width: 12rem;
+  height: 0.3rem;
+  // Use radius and overflow to limit inner progress bar radius
+  border-radius: 0.125rem;
+  overflow: hidden;
+}
+
+.progress-bar-placeholder {
+  position: absolute;
+  width: inherit;
+  height: inherit;
+  border-radius: 0.125rem;
+  border: 1px solid #3d3d3d;
+  background-color: #262626;
+}
+
+.progress-bar-progress {
+  position: absolute;
+  // width: 10rem;
+  height: inherit;
+  background-color: #cccccc;
+}
 </style>
